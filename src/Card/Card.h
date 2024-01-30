84aa4007b8c64778c3d842e05ff2794e866076ff
creibaud <creibaud@guardiaschool.fr>
2024-01-30 12:51:43 +0100

    Remove unused include and type definition in Card.h

--- src/Card/Card.h
+++ src/Card/Card.h
@@ -2,7 +2,6 @@
 #define CARD_H
 
 #include <string>
-#include <variant>
 #include "../includes/TypeCard/TypeCard.h"
 #include "../includes/TypeCard/TypeRoleCard.h"
 #include "../includes/TypeCard/TypeActionCard.h"
@@ -10,7 +9,6 @@
 #include "../includes/TypeCard/TypeCharacterCard.h"
 #include "../includes/TypeCard/TypeWeaponCard.h"
 
-using TypeName = std::variant<TypeRoleCard, TypeActionCard, TypePermanentCard, TypeCharacterCard, TypeWeaponCard>;
 class Card {
     private:
         TypeCard type;
